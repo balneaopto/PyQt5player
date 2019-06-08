7fbf783249714fc54a6b28cb7bc9d9c35e359824
balneaopto <51031809+balneaopto@users.noreply.github.com>
2019-06-08 22:45:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@
 # -*- coding: utf-8 -*-
 # Part of myplayer. Configure player here!
 # Relative path, relative to your home directory, of the directory containing your MP3s collection
-mp3dir = "/playlist01/"
-#mp3dir = "/Musica/"
+#mp3dir = "/playlist01/"
+#mp3dir = "/Music/"
+##mp3dir = "/Music/myplaylist/"
