245fb01393db6a84e74229973dfb7d3b9728cdca
balneaopto <51031809+balneaopto@users.noreply.github.com>
2019-06-08 22:43:58 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Part of myplayer. Configure player here!
+# Relative path, relative to your home directory, of the directory containing your MP3s collection
+mp3dir = "/playlist01/"
+#mp3dir = "/Musica/"
